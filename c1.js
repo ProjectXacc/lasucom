80d4ba4a4d1fd5504b3f720b9b5cf0f1e9dad2b3
ProjectXacc <de_realjay@yahoo.com>
2024-12-01 11:28:07 +0100

    Update c1.js

--- c1.js
+++ c1.js
@@ -856,8 +856,10 @@ document.getElementById('goButton').addEventListener('click', () => {
 });
 // Password configuration with manually set expiration dates
 const passwords = {
-    "pin": new Date("2024-12-01T23:59:59").getTime(), // Custom expiration date and time
+    "OGUNLEYE": new Date("2025-12-01T23:59:59").getTime(), // Custom expiration date and time
  "PADONU1": new Date("2024-12-01T23:59:59").getTime(), // Custom expiration date and time
+  "OGUNLEYE": new Date("2025-12-01T23:59:59").getTime(), // Custom expiration date and time
+  "OGUNLEYE": new Date("2025-12-01T23:59:59").getTime(), // Custom expiration date and time
     "car": new Date("2024-12-15T23:59:59").getTime()  // Custom expiration date and time
 };
 
